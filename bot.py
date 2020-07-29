80bbe7378c754c5d4b9e9dba7b97ee1443befc1d
sh1rokovs <hilallod@yandex.ru>
2020-07-29 16:30:58 +0300

    zz1

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import telebot
 from telebot import types
 
-bot = telebot.TeleBot('1106764268:AAGYI5tr6pWv_apqhEVq8hXAWZZQ_ie0zR8')
+bot = telebot.TeleBot('api-key')
 
 
 @bot.message_handler(commands=['website'])
