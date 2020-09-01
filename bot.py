9c142ea21d75ec79570cd13df9656c249dd102d4
sh1rokovs <hilallod@yandex.ru>
2020-09-01 18:43:07 +0300

    zz

--- bot.py
+++ bot.py
@@ -1,45 +1,49 @@
 import telebot
 from telebot import types
 
-bot = telebot.TeleBot('api-key')
+bot = telebot.TeleBot('1106764268:AAGYI5tr6pWv_apqhEVq8hXAWZZQ_ie0zR8')
 
 
 @bot.message_handler(commands=['website'])
 def open_website(message):
     markup = types.InlineKeyboardMarkup()
-    markup.add(types.InlineKeyboardButton("Перейти на сайт", url="https://itproger.com"))
+    markup.add(types.InlineKeyboardButton("Перейти к оплате", url="https://www.free-kassa.ru"))
     bot.send_message(message.chat.id,
-                     "Отличный выбор, нажмите на кнопку ниже и начинайте изучения курсов прямо сейчас",
+                     "Отличный выбор, нажмите на кнопку ниже и купите решение",
                      parse_mode='html', reply_markup=markup)
 
 
 @bot.message_handler(commands=['insta'])
 def instagram(message):
     markup = types.InlineKeyboardMarkup()
-    markup.add(types.InlineKeyboardButton("Перейти в Инстаграм", url="https://www.instagram.com/itproger_official/"))
-    bot.send_message(message.chat.id, "Нажмите на кнопку ниже и погрузитесь в мир IT прямо сейчас", parse_mode='html',
+    markup.add(types.InlineKeyboardButton("Перейти в Инстаграм", url="https://www.instagram.com"))
+    bot.send_message(message.chat.id, "Нажмите на кнопку ниже и посмотрите отзывы в инсте", parse_mode='html',
                      reply_markup=markup)
 
 
 @bot.message_handler(commands=['vk'])
 def vk(message):
     markup = types.InlineKeyboardMarkup()
-    markup.add(types.InlineKeyboardButton("Посетить группу Вк", url="https://vk.com/prog_life"))
-    bot.send_message(message.chat.id, "Нажмите на кнопку ниже и погрузитесь в мир IT прямо сейчас", parse_mode='html',
+    markup.add(types.InlineKeyboardButton("Посетить группу Вк", url="https://vk.com"))
+    bot.send_message(message.chat.id, "Нажмите на кнопку ниже и посмотрите отзывы в вк", parse_mode='html',
                      reply_markup=markup)
 
 
 @bot.message_handler(commands=['start'])
 def start(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=3)
-    btn1 = types.KeyboardButton('Создание игр')
-    btn2 = types.KeyboardButton('Мобильные приложения')
-    btn3 = types.KeyboardButton('Веб разработка')
-    btn4 = types.KeyboardButton('Софт для компьютеров')
-    btn5 = types.KeyboardButton('Обработка данных')
-    btn6 = types.KeyboardButton('Создание ИИ')
-    markup.add(btn1, btn2, btn3, btn4, btn5, btn6)
-    send_mess = f"<b>Привет {message.from_user.first_name} {message.from_user.last_name}</b>!\nКакое направление тебя интересует?"
+    btn1 = types.KeyboardButton('Купить решение')
+    btn2 = types.KeyboardButton('Инста')
+    btn3 = types.KeyboardButton('Вк')
+    markup.add(btn1, btn2, btn3)
+    send_mess = f"<b>Привет {message.from_user.first_name} {message.from_user.last_name}</b>!\n" \
+                f" Отличная способ заработать денюшку, играя в игру!\n" \
+                f" Все максимально просто и честно!\n" \
+                f" Вывод средств момементально, любым способом!\n" \
+                f" 1. Вложить в биткоин\n" \
+                f" 2. Купить схему\n" \
+                f" 3. Начать зарабатывать\n" \
+                f" Какое решение тебя интересует?\n"
     bot.send_message(message.chat.id, send_mess, parse_mode='html', reply_markup=markup)
 
 
@@ -47,41 +51,47 @@ def start(message):
 def mess(message):
     get_message_bot = message.text.strip().lower()
 
-    if get_message_bot == "начать тест заново":
+    if get_message_bot == "купить решение":
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=3)
-        btn1 = types.KeyboardButton('Создание игр')
-        btn2 = types.KeyboardButton('Мобильные приложения')
-        btn3 = types.KeyboardButton('Веб разработка')
-        btn4 = types.KeyboardButton('Софт для компьютеров')
-        btn5 = types.KeyboardButton('Обработка данных')
-        btn6 = types.KeyboardButton('Создание ИИ')
-        markup.add(btn1, btn2, btn3, btn4, btn5, btn6)
-
-        final_message = "Решил попробовать что-то ещё? \nВыбери какое направление тебя интересует:"
-    elif get_message_bot == "создание игр":
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=3)
-        btn1 = types.KeyboardButton('Под мобильные телефоны')
-        btn2 = types.KeyboardButton('Компьютеры и консоли')
-        btn3 = types.KeyboardButton('Виртуальная реальность')
-        btn4 = types.KeyboardButton('Web игра')
-        btn5 = types.KeyboardButton("Начать тест заново")
-        markup.add(btn1, btn2, btn3, btn4, btn5)
-        final_message = "Отлично, геймдев крутая тема, но под что хочется разрабатывать?"
-    elif get_message_bot == "под мобильные телефоны":
+        btn1 = types.KeyboardButton('Купить решение')
+        btn2 = types.KeyboardButton('Инста')
+        btn3 = types.KeyboardButton('Вк')
+        markup.add(btn1, btn2, btn3)
         markup = types.InlineKeyboardMarkup()
-        markup.add(types.InlineKeyboardButton("Посмотреть курсы по Unity", url="https://itproger.com/tag/unity"))
-        final_message = "Для разработки игр под мобильные устройства зачастую используется игровой движок <a href='https://itproger.com/tag/unity'>Unity</a>\nДвижок прост в изучении и вы можете просмотреть курсы по нему по кнопке ниже"
-    # Здесь различные дополнительные проверки и условия
+        markup.add(types.InlineKeyboardButton("Купить решение", url="https://www.free-kassa.ru"))
+
+        final_message = "Вы можете купить разные решения на <a href='https://www.free-kassa.ru'>Сайте</a>\n" \
+                        "по кнопке ниже"
+    elif get_message_bot == "инста":
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=3)
+        btn1 = types.KeyboardButton('Купить решение')
+        btn2 = types.KeyboardButton('Инста')
+        btn3 = types.KeyboardButton('Вк')
+        markup.add(btn1, btn2, btn3)
+        markup = types.InlineKeyboardMarkup()
+        markup.add(types.InlineKeyboardButton("Посмотреть отзывы", url="https://www.instagram.com"))
+
+        final_message = "Вы можете также посмотреть наши отзывы в <a href='https://www.instagram.ru'>Inste</a>\n" \
+                        "по кнопке ниже"
+    elif get_message_bot == "вк":
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=3)
+        btn1 = types.KeyboardButton('Купить решение')
+        btn2 = types.KeyboardButton('Инста')
+        btn3 = types.KeyboardButton('Вк')
+        markup.add(btn1, btn2, btn3)
+        markup = types.InlineKeyboardMarkup()
+        markup.add(types.InlineKeyboardButton("Посмотреть отзывы", url="https://www.vk.com"))
+
+        final_message = "Вы можете также посмотреть наши отзывы в <a href='https://www.vk.com'>Вк</a>\n" \
+                        "по кнопке ниже"
     else:
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=3)
-        btn1 = types.KeyboardButton('Создание игр')
-        btn2 = types.KeyboardButton('Мобильные приложения')
-        btn3 = types.KeyboardButton('Веб разработка')
-        btn4 = types.KeyboardButton('Софт для компьютеров')
-        btn5 = types.KeyboardButton('Обработка данных')
-        btn6 = types.KeyboardButton('Создание ИИ')
-        markup.add(btn1, btn2, btn3, btn4, btn5, btn6)
-        final_message = "Так, так, так\nПостой, лучше нажми на одну из интерактивных кнопок ниже"
+        btn1 = types.KeyboardButton('Купить решение')
+        btn2 = types.KeyboardButton('Инста')
+        btn3 = types.KeyboardButton('Вк')
+        markup.add(btn1, btn2, btn3)
+        final_message = "Вы можете также посмотреть наши отзывы в <a href='https://www.vk.com'>Вк</a>\n" \
+                        "по кнопке ниже"
     bot.send_message(message.chat.id, final_message, parse_mode='html', reply_markup=markup)
 
 
